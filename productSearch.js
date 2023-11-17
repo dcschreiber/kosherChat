944bc908779551dac90f0139f1294ac1e2b2ffd1
dcschreiber <daniel1schreiber@gmail.com>
2023-11-17 15:49:56 -0500

    Setting up index with algolia

--- productSearch.js
+++ productSearch.js
@@ -1,56 +1,58 @@
+const algoliasearch = require('algoliasearch');
 const { toLog } = require('./libs/logger.js');
 const { connectToDB, closeDBConnection } = require('./libs/dbConnection');
+const dotenv = require('dotenv');
+require('dotenv').config();
+dotenv.config();
 
-const dbName = 'kosherLists';
-const collectionName = 'KLBD8822';
+let client, index;
+if (process.env.ENV === 'production') {
+    client = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
+    index = client.initIndex(process.env.YOUR_ALGOLIA_INDEX_NAME);
+}
+
+async function searchWithAlgolia(productName) {
+    if (!client || !index) {
+        throw new Error('Algolia is not configured');
+    }
+    try {
+        const searchResults = await index.search(productName, { attributesToRetrieve: ['product', 'kosherSearchIndex'], hitsPerPage: 10 });
+        return searchResults.hits; // This will return an array of search results
+    } catch (error) {
+        toLog(`Error in searchWithAlgolia: ${error.message}`);
+        throw error; // Rethrow the error to handle it in the calling function
+    }
+}
 
 async function findProduct(productName) {
     let results = { count: 0, products: [] };
-    let db, collection;
 
     try {
-        db = await connectToDB(dbName);
-
         if (process.env.ENV === 'production') {
-            // Firestore-specific logic
-            collection = db.collection(collectionName);
-            // Perform the query
-            const querySnapshot = await collection.where('product', '==', productName).limit(10).get();
-
-            // Check the size of the results
-            if (querySnapshot.size > 10) {
-                toLog("Please try a more specific query");
-            } else {
-                querySnapshot.forEach((doc) => {
-                    results.products.push(doc.data());
-                });
-                results.count = results.products.length;
-                toLog(`productSearch: Number of documents that match the query: ${results.count}`);
-            }
-
+            // Use Algolia for textual search in production
+            results.products = await searchWithAlgolia(productName);
+            results.count = results.products.length;
         } else {
-            // MongoDB-specific logic
-            collection = db.collection(collectionName);
+            // Fallback to local DB search logic for development/testing
+            const db = await connectToDB();
+            const collection = db.collection('your-collection-name');
 
             const query = { $text: { $search: productName } };
             results.count = await collection.countDocuments(query);
-            toLog(`Number of documents that match the query: ${results.count}`);
 
             if (results.count < 10) {
-                const productsCursor = collection.find(query);
-                results.products = await productsCursor.toArray();
+                results.products = await collection.find(query).toArray();
             } else {
                 toLog("Please try a more specific query");
             }
+            await closeDBConnection();
         }
     } catch (e) {
         console.error(e);
         toLog(`Error in findProduct: ${e.message}`);
-    } finally {
-        await closeDBConnection();
     }
 
     return results;
 }
 
-module.exports.findProduct = findProduct;
+module.exports = { findProduct };
